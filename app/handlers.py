84b85c94fb378998da2955e50c2df30df807fa37
dethmet <mpgdethmet48@gmail.com>
2025-02-01 17:53:59 +0300

    new bot: create sneakers shop

--- app/handlers.py
+++ app/handlers.py
@@ -1,9 +1,12 @@
 
 from aiogram import F, Router
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
 
 import app.keyboards as kb
+import app.database.requests as rq
 
 
 router = Router()
@@ -11,28 +14,22 @@ router = Router()
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.reply(f'Привет.\nТвой ID: {message.from_user.id}\nИмя: {message.from_user.first_name}',
-                        reply_markup=await kb.inline_pidors())
+    await rq.set_user(message.from_user.id)
+    await message.answer('Добро пожаловать в магазин кросовок', reply_markup=kb.main)
 
-
-@router.message(Command('help'))
-async def get_help(message: Message):
-    await message.answer('Бог помощь тебе /help')
-
-
-@router.message(F.text == 'Как дела, братка?')
-async def how_are_you(message: Message):
-    await message.answer('Пока не родила. АХАХАХА')
-
-
-@router.message(Command('get_photo'))
-async def get_photo(message: Message):
-    await message.answer_photo(
-        photo='https://sun9-1.userapi.com/impf/Ls-1OT6a6X1FLnBSnXZKcV1FhRePPrGHzAIrQw/xQySqmfBtPU.jpg?size=700x607&quality=96&sign=bfc9558697717136c0b6aa620131248e&c_uniq_tag=eO7YMvdUZcUkgVCrDIT6Hb56sJBZOLhzkxIGKippHB8&type=album.jpg',
-        caption='Это та крутая твоя фотка в лифте'
-    )
+@router.message(F.text == 'Каталог')
+async def catalog(message: Message):
+    await message.answer('Выберите категорию товара', reply_markup=await kb.categories())
     
-
-@router.message(F.photo)
-async def get_photo(message: Message):
-    await message.answer(f'ID фото: {message.photo[-1].file_id}')
+@router.callback_query(F.data.startswith('category_'))
+async def category(callback: CallbackQuery):
+    await callback.answer('Вы выбрали категоритю')
+    await callback.message.answer('Выберите товар по категории',
+                                  reply_markup=await kb.items(callback.data.split('_')[1]))
+    
+@router.callback_query(F.data.startswith('item_'))
+async def category(callback: CallbackQuery):
+    item_data = await rq.get_item(callback.data.split('_')[1])
+    await callback.answer('Вы выбрали товар')
+    await callback.message.answer(f'Название: {item_data.name}\nОписание: {item_data.description}\nЦена: {item_data.price}$',
+                                  reply_markup=await kb.items(callback.data.split('_')[1]))
--- run.py
+++ run.py
@@ -1,22 +1,20 @@
 import asyncio
-import logging
-
 from aiogram import Bot, Dispatcher
 
-from config import TOKEN
 from app.handlers import router
-
-bot = Bot(token=TOKEN)
-dp = Dispatcher()
-
+from app.database.models import async_main
 
 async def main():
+    await async_main()
+    bot = Bot(token='7701651173:AAFexhooXveVPbD2tZkHUb05K16orX16ASY')
+    dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot)
 
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
+
+if __name__ == '__main__':
     try:
-        asyncio.run(main())        
+        asyncio.run(main())
     except KeyboardInterrupt:
-        print('Выход')
+        print('Бот выключен')
+
